e9c340bc15f4192409e5d8bac63e716cb58ee25f
salemovic <jakihop889@gmail.com>
2022-09-20 09:34:21 +0300

    Added The Main Folder Of Data Structure That Contain Four Folder Linked list And stack and queue and tree

---
+++ DataStructure/LinkedList/LinkedList.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.LinkedLists
+{
+
+    class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node()
+        {
+
+        }
+        public Node(int value)
+        {
+            Data = value;
+        }
+        
+    }
+
+
+    internal class LinkedList
+    {
+        Node head = null;
+
+        //Remove All Item in Linked List
+        public void Clear()
+        {
+            head = null;
+        }
+        //Check if Linked List Empty
+        private bool IsNUll(Node head)
+        {
+           return head == null;
+        }
+        //Return Number item of Linked List
+        public int Count()
+        {
+            int count = 0;
+
+            Node current = head;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+            return count;
+        }
+        //Add New Item
+        public  void insertAtEnd(int value)
+        {
+            Node NewNode = new Node(value);
+
+            //case one if Linked List has no itmes
+            if (IsNUll(head))
+            {
+                head = NewNode;
+                NewNode.Next = null;
+                return;
+            }
+
+            //case two if Linked List Has Many items
+            Node current = head;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = NewNode;
+            NewNode.Next = null;
+        }
+        public void insertAtBegging(int value)
+        {
+            Node NewNode = new Node(value);
+
+            NewNode.Next = head;
+            head = NewNode;
+            
+        }
+        public void RemoveByValue(int value)
+        {
+            //Point To First Node
+            Node current = head;
+            Node privous = head;
+
+            //if value Found in first node
+            if(current.Data == value)
+            {
+                head = current.Next;
+                return;
+            }
+
+            while (current.Data != value)
+            {
+                privous = current;
+                current = current.Next;
+            }
+
+            privous.Next = current.Next;
+        }
+        public void RemoveAtBegging()
+        {
+            if(IsNUll(head))
+            {
+                Console.WriteLine("Linked List Is Empty!");
+                return;
+            }
+
+            head = head.Next;
+        }
+        public void RemoveAtEnd()
+        {
+            if(IsNUll(head))
+            {
+                Console.WriteLine("Linked List is Empty!");
+                return;
+            }
+
+            if(IsNUll(head.Next))
+            {
+                head = null;
+                return;
+            }
+
+            Node PTR = head;
+
+            while (PTR.Next.Next != null)
+            {
+                PTR = PTR.Next;
+            }
+            PTR.Next = null;
+        }
+        public  void Display()
+        {
+            //case one if Linked List Empty print message
+            if (IsNUll(head))
+            {
+                Console.WriteLine("Linked List is Empty !");
+                return;
+            }
+
+            //case two if Liked List Has items
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+
+        }
+    }
+
+}
---
+++ DataStructure/Stack/Stack.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Stacks
+{
+    //using array
+    internal class myStack
+    {
+        public int[] Array { get; set; }
+        public int Size { get; set; }
+        public int Top { get; set; }
+
+        public myStack(int size)
+        {
+            Size = size;
+            Array = new int[Size];
+            Top = -1;
+        }
+        public int Count()
+        {
+            return Top + 1;
+        }
+        public bool IsEmpty()
+        {
+            return Top == -1;
+        }
+        public bool IsFull()
+        {
+            return Top == Array.Length - 1;
+        }
+        public void Push(int value)
+        {
+            if(IsFull())
+            {
+                Console.WriteLine("Stack OverFlow!");
+                return;
+            }
+
+            Array[++Top] = value;
+        }
+        public void Pop()
+        {
+            if(IsEmpty())
+            {
+                Console.WriteLine("Stack is Empty!");
+                return;
+            }
+
+            Top--;
+
+        }
+        public int Peek()
+        {
+            return IsEmpty() ? -1 : Array[Top];
+        }
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is Empty!");
+                return;
+            }
+            for (int i = Top; i >= 0; i--)
+            {
+                Console.Write(Array[i] + " ");
+            }
+        }
+    }
+
+
+    class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+
+    //Using Linked List
+    class Stack
+    {
+        public Node Top = null;
+        public bool IsNull(Node top)
+        {
+            return Top == null;
+        }
+        public void Message()
+        {
+            Console.WriteLine("Stack is Empty!");
+
+        }
+        public void Push(int value)
+        {
+            Node NewNode = new Node(value);
+            if(IsNull(Top))
+            {
+                Top = NewNode;
+                return;
+            }
+            NewNode.Next = Top;
+            Top = NewNode;
+        }
+        public void Pop()
+        {
+            if(IsNull(Top))
+            {
+               Message();
+                return;
+            }
+
+            Top = Top.Next;
+        }
+        public int Peek()
+        {
+            return IsNull(Top) ? -1 :  Top.Value;
+        }
+        public void Display()
+        {
+            if(IsNull(Top))
+            {
+                Message();
+                return;
+            }
+
+            Node current = Top;
+
+            while(current != null)
+            {
+                Console.WriteLine(current.Value + " ");
+                current = current.Next;
+            }
+        }
+    }
+}
